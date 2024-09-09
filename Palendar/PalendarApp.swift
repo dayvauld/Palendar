b9c445050cdff12f93cb98e5b63c19fb3f16ce51
David Auld <david.a.auld@gmail.com>
2024-09-09 13:11:48 -0700

    Initial Commit

---
+++ Palendar/PalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  PalendarApp.swift
+//  Palendar
+//
+//  Created by David Auld on 2024-09-09.
+//
+
+import SwiftUI
+
+@main
+struct PalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
